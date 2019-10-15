ef517b0d30420a17be55a3a4b8795c99f95f52f6
Reakain <reakain@users.noreply.github.com>
2019-10-15 13:48:32 -0400

    initial commit of hide and seek with base sprites and animations, player movement, and chaser base level flee and chase logic

---
+++ Hide-And-Seek/Assets/HideAndSeek/Scripts/ChaserController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ChaseState
+{
+    None,
+    Hide,
+    Found,
+    Flee,
+    Chase,
+    Caught
+}
+
+public class ChaserController : MonoBehaviour
+{
+    public float movementSpeed = 4;
+    Vector2 lookDirection;
+    public ChaseState currentState = ChaseState.None;
+
+    Rigidbody2D rigidbody2d;
+    Animator animator;
+
+    private void Awake()
+    {
+        currentState = ChaseState.Flee;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        switch(currentState)
+        {
+            case ChaseState.None:
+                break;
+            case ChaseState.Hide:
+                break;
+            case ChaseState.Found:
+                break;
+            case ChaseState.Flee:
+                ChaseFlee();
+                break;
+            case ChaseState.Chase:
+                ChaseFlee();
+                break;
+            case ChaseState.Caught:
+                break;
+        }
+        
+    }
+
+    public void ChaseFlee()
+    {
+        var speed = movementSpeed * Time.deltaTime;
+        speed = ((currentState == ChaseState.Chase)? 1 : -1)*speed; // Move towards or away?
+        Vector2 move = Vector2.MoveTowards(rigidbody2d.position, CharacterController.instance.transform.position, speed);
+        SetLook(move);
+
+        // Get your current position
+        //Vector2 position = rigidbody2d.position;
+
+        // Set your position as your position plus your movement vector, times your speed multiplier, and the current game timestep;
+        //position = position + move * speed * Time.deltaTime;
+
+        // Tell the rigidbody to move to the positon specified
+        rigidbody2d.MovePosition(move);
+    }
+
+    void SetLook(Vector2 move)
+    {
+        // If your moving, set your look direction as move direction and normalize the vector (set it to magnitude of 1)
+        if (!Mathf.Approximately(transform.position.x, 0.0f) || !Mathf.Approximately(transform.position.y, 0.0f))
+        {
+            lookDirection.Set(transform.position.x, transform.position.y);
+            lookDirection.Normalize();
+        }
+
+        // Set your animator animation direction and speeds
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Hide And Seek
+[![Build Status](https://api.travis-ci.com/reakain/Hide-And-Seek.svg?branch=master)](https://travis-ci.com/reakain/Hide-And-Seek)
+[![unity version](https://img.shields.io/badge/unity%20version-2019.1.14f1-green.svg)]()
+
+Unity build out for 2D top down hide and seek mini game.
+
+## What is?
+Designed as top down hide and seek game. 
+
+## To-Do
+ - State change logic
+ - obstacle handling
+ - hiding
+ - tagging
+
+## References
